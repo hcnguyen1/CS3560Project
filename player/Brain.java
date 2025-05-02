94fe9aea46a6e104a4e4567779c15edf08b8012d
Tavina Chen <tavinachen.work@gmail.com>
2025-05-01 17:16:37 -0700

    player class + brain rich strat

--- player/Brain.java
+++ player/Brain.java
@@ -1,3 +1,89 @@
 package player;
+import terrain.Path;
+import terrain.Cost;
 
-public class Brain {}
+public class Brain {
+
+    int waterThreshold = 5;
+    int foodThreshold = 5;
+    int goldThreshold = 1;
+    int energyThreshold = 5;
+    Path currentPath;
+    double n = 1.5; //multiplier 
+    Player player;
+    Vision vision;
+
+    public Brain (Player p) {
+        player = p;
+    }
+
+    public void setVision(Vision v) {
+        vision = v;
+    }
+
+    public void makeMove() {
+        int water = player.getWaterAmount();
+        int food = player.getFoodAmount();
+        int energy = player.getEnergyAmount();
+        int gold = player.getGoldAmount();
+
+        //determine which strategy to use
+        if (food >= foodThreshold && water >= waterThreshold) { //both food and water is above threshold
+            if (energy < energyThreshold) {
+                this.energyConversion();
+            } else {
+                this.richStrategy();
+            }
+        } else if (food <= foodThreshold && water <= waterThreshold) { //both food and water is below threshold
+            this.scavengerStrategy();
+        } else { //one is below threshold, one is above
+
+            //there is n times more water than food or food than water or player has gold to spend
+            if (food >= n*water || water >= n*food || gold > goldThreshold) {
+                this.equalizerStrategy();
+            } else { //the player has about an equal amount of food and water but one is below the threshold
+                this.lowOnOneStrategy();
+            }
+        }
+
+    }
+
+    //use half the costs of the terrain where the player is staying
+    private void stay() {
+        Cost c = player.getCurrentTerrain().getCost();
+        Cost.half(c);
+        player.useCost(c);
+    }
+
+    private void energyConversion() {
+        this.stay();
+    }
+
+    private void scavengerStrategy() {
+        
+    }
+
+    private void equalizerStrategy() {
+        
+    }
+
+    private void lowOnOneStrategy() {
+        
+    }
+
+    //Take the easiest path
+    //generates a new easiest path when currentPath is null, otherwise continue on the path
+    private void richStrategy() {
+        if (currentPath == null || currentPath.getNumSteps == 0) {
+            Path paths[] = vision.easiestPath();
+            currentPath = paths[0];
+            currentPath.takePath();
+        } else { 
+            currentPath.takePath();
+        }
+
+        //use the bonuses at the terrain the player enters
+        //the path object takes care of the costs
+        player.useBonus();
+    }
+}
--- player/Player.java
+++ player/Player.java
@@ -1,3 +1,100 @@
 package player;
+import java.util.Map;
+import java.util.Random;
+import terrain.Terrain;
+import bonus.Bonus;
+import terrain.Cost;
+//import default.Difficulty;
 
-public class Player {}
+public class Player {
+    //insert private variable here
+    private Vision vision;
+    private Brain brain = new Brain(this);
+    private int gold = 0;
+    private int water = 15;//maxWater;
+    private int food = 15; //maxFood;
+    private int energy = 15; //maxEnergy;
+
+    //player location
+    private int locationX = 0;
+    private int locationY = 3;//Map.getSize()/2;
+
+    //constructor
+    public Player() {
+        //Randomize which vision to get
+        Random rng = new Random();
+        int visionType = rng.nextInt(4) + 1;
+        
+        vision = switch (visionType) {
+            case 2 -> new CautiousVision();
+            case 3 -> new KeenEyedVision();
+            case 4 -> new FarSight();
+            default -> new FocusedVision();
+        }; //case 1
+
+        brain.setVision(vision);
+    }
+
+    public void makeMove() {
+        brain.makeMove();
+    }
+
+    public Vision getVision() {
+        return vision;
+    }
+
+    public Terrain getCurrentTerrain() {
+        Map.getTerrain(locationX, locationY);
+    }
+
+    public int getX() {
+        return locationX;
+    }
+
+    public int getY() {
+        return locationY;
+    }
+
+    //Uses the bonuses if it does not exceed the maximum
+    public void useBonus() {
+        Bonus b = this.getCurrentTerrain().getBonus();
+        //if it doesn't go over the max, then use the bonus
+        if ((gold + b.getGold() <= maxGold)) {
+            gold += b.getGold();
+            b.useGold(); //depleates the gold from the bonus
+        }
+        
+        if ((water + b.getWater() <= maxWater)) {
+            water += b.getWater();
+            b.useWater();
+        }
+       
+        if ((food + b.getFood() <= maxFood)) {
+            food += b.getFood();
+            b.useFood();
+        }
+    }
+
+    public void useTrader() {
+        Bonus b = this.getCurrentTerrain().getTrader();
+        //TBC
+    }
+
+    public void useCost(Cost c) {
+        //TBC later
+    }
+
+    public int getGoldAmount(){
+        return gold;
+    }
+    public int getWaterAmount(){
+        return water;
+    }
+    public int getFoodAmount(){
+        return food;
+    }
+    public int getEnergyAmount(){
+        return energy;
+    }
+
+}
