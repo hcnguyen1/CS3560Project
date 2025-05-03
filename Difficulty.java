5ea795e9a89a94259729fbb6db33a5143dacc449
jqlnet <toniliang@cpp.edu>
2025-05-02 18:41:19 -0700

    Update Difficulty.java
    added difficulty, still needs trader and other abstract methods to add

--- Difficulty.java
+++ Difficulty.java
@@ -1,11 +1,8 @@
-public class Difficulty {
+public interface Difficulty {
+    double getPlainsSpawnRate();
+    double getForestSpawnRate();
+    double getMountainSpawnRate();
+    double getSwampSpawnRate();
+    double getDesertSpawnRate();
 
-  // Placeholder code
-
-  private int[] difficultylevels = { 1, 2, 3 }; // 1 = Easy, 2 = Medium, 3 = Hard
-  private int selectedDifficulty = difficultylevels[1]; // Default to Easy
-
-  public int getDifficultyLevel() {
-    return selectedDifficulty;
-  }
 }
