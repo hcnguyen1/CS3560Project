e13f607d9c6df03f4f2df4a2db555634cb8e54b2
Hao <120632700+hcnguyen1@users.noreply.github.com>
2025-05-04 18:04:09 -0700

    Resource + Terrain Update

--- bonus/Bonus.java
+++ bonus/Bonus.java
@@ -1,3 +1,21 @@
 package bonus;
 
-public interface Bonus {}
+public interface Bonus {
+  // Getters for the bonus values
+  public int getGold();
+
+  public int getWater();
+
+  public int getFood();
+
+  public int getEnergy();
+
+  // Player calls these method to deplete the bonus from the terrain
+  public void useGold();
+
+  public void useWater();
+
+  public void useFood();
+
+  public void useEnergy();
+}
